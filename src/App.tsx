8522c4b82b7f8fb2486a705ce655d9853ce14ef0
Kami <kamdin@web.de>
2024-10-15 15:19:58 +0200

    -kami-

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,39 @@
 import { useState } from "react";
-
-export interface Weather {
-  coord: Coord;
-  weather: WeatherElement[];
-  base: string;
-  main: Main;
-  visibility: number;
-  wind: Wind;
-  clouds: Clouds;
-  dt: number;
-  sys: Sys;
-  timezone: number;
-  id: number;
-  name: string;
-  cod: number;
-}
-
-export interface Clouds {
-  all: number;
-}
-
-export interface Coord {
-  lon: number;
-  lat: number;
-}
-
-export interface Main {
-  temp: number;
-  feels_like: number;
-  temp_min: number;
-  temp_max: number;
-  pressure: number;
-  humidity: number;
-  sea_level: number;
-  grnd_level: number;
-}
-
-export interface Sys {
-  type: number;
-  id: number;
-  country: string;
-  sunrise: number;
-  sunset: number;
-}
-
-export interface WeatherElement {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
-
-export interface Wind {
-  speed: number;
-  deg: number;
-}
+import { Weather } from "./Weather";
 
 function App() {
-  const APIkey: string = "88e707da98c1627f8233021ca9991abc";
+  const APIkey: string = import.meta.env.VITE_API_KEY;
   const country: string = "DE" || "AU";
-  const city: string = "Hamburg" || "Berlin " || "Cologne" || "Sydney";
 
-  const [weather, setWeather] = useState(false);
   const [weatherData, setWeatherData] = useState<Weather | null>(null);
 
   const handleClick = (e: any) => {
     e.preventDefault();
     let currentCity = e.target.innerText;
-    setWeather(true);
-    if (weather) {
-      fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${currentCity},${country}&appid=${APIkey}`
-      )
-        .then((res) => res.json())
-        .then((res) => setWeatherData(res))
-        .catch((err) => console.log(err));
-    }
+    
+
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${currentCity},${country}&appid=${APIkey}`
+    )
+      .then((res) => res.json())
+      .then((res) => setWeatherData(res))
+      .catch((err) => console.log(err));
   };
+
   const handleAUClick = (e: any) => {
     e.preventDefault();
     let currentCity = e.target.innerText;
     const country: string = "AU";
-    setWeather(true);
-    if (weather) {
-      fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${currentCity},${country}&appid=${APIkey}`
-      )
-        .then((res) => res.json())
-        .then((res) => setWeatherData(res))
-        .catch((err) => console.log(err));
-    }
+
+ 
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${currentCity},${country}&appid=${APIkey}`
+    )
+      .then((res) => res.json())
+      .then((res) => setWeatherData(res))
+      .catch((err) => console.log(err));
   };
+
   const minusKelvin: number = 273.15;
 
   return (
